9b82704bbbc3f25dd7fd7ee80fd2713e039eadbe
kingkingburger <dnjsalsgh123@gmail.com>
2021-07-25 14:57:22 +0900

    21/07/25 해결

--- 2884.py
+++ 2884.py
@@ -1,12 +1,12 @@
 h,m = map(int, input().split(' '))
+if(m < 45):
+    h -= 1
 m += 15
-h -= 1
-print(m)
-if(m == 60 or 65 or 70 or 75):
-    m = m - 45
-if(h <0):
+
+
+if(m >= 60 ):
+    m -=  60
+if(h < 0):
     h = 23
 
 print(h, m)
-
-
