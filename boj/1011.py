379bc4adf58f96edad196b656959dd12b0b14c0f
kingkingburger <dnjsalsgh123@gmail.com>
2021-09-30 18:13:47 +0900

    [python 백준 1011]

--- boj/1011.py
+++ boj/1011.py
@@ -1,18 +1,26 @@
-test_case = int(input())
+import math
 
-for i in range(test_case):
-    x,y = map(int, input().split())
+t = int(input())
 
-    distance = y-x
-    count = 0 # 이동 횟수
-    move = 1 # 이동 가능 거리
-    move_count = 0 #이동 거리 합
-    while(move_count < distance):
-        count +=1
-        move_count += move
-        if count % 2 ==0:
-            move +=1
+for i in range(t):
+    x, y = map(int, (input().split()))
+    distance = y-x                         #주어진 값들간의 거리
+
+    num = math.floor(math.sqrt(distance))  #주어진 값들 사이의 거리에 루트 씌움 (제곱근) , floor처리되어 이미 정수임    
+    num_jegob = num**2                # 정수를 제곱근으로 갖는 제곱수(ex. 9 : 9의 제곱근은 3)
+
+    if distance == num_jegob:
+        count = (num*2)-1
+
+    elif num_jegob < distance <= num_jegob + num:
+        count = (num*2)
+
+    elif (num_jegob + num) < distance:
+        count = (num*2) + 1
+
+    if distance < 4:
+        count = distance
+    
     print(count)
-        
 
 
