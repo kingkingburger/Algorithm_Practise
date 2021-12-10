73163e5aeb63dbd26ada4232e7f63d007d905aaf
kingkingburger <dnjsalsgh123@gmail.com>
2021-12-10 22:58:29 +0900

    [python 백준 15649]

--- boj/15649.py
+++ boj/15649.py
@@ -1,20 +1,18 @@
 n, m = map(int, input().split())
-# m = 깊이
-base = []
-visited = [False for _ in range(n)]
-for i in range(1, n+1):
-    for j in range(1, m +1):
-        base.append(j)
-    base.append(i)
 
-print(base)
-def dfs(base, v, visited):
-    for i in range(n):
-        visited[i] = True
-        print(v+1, end=' ')
-    
-    for i in base:
-        if not visited[i]:
-            dfs(base, i, visited)
+base = [] # 재귀함수를 이용하여 m개의 수열을 저장하기 위한 리스트
 
-dfs(base, 2, visited)
+def dfs():
+    #리스트에 들어간 수열들이 m개가 되면
+    #리스트에 들어있는 숫자들을 모두 출력하고 함수를 나온다.
+    if(len(base) == m):                
+        print(' '.join(map(str,base)))
+        return
+
+    for i in range(1, n+1):
+        if i not in base:  #리스트 s 중복여부 확인
+            base.append(i)
+            dfs()
+            base.pop()
+
+dfs()
