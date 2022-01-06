975dfa6c4b729a7548797ace2ceed11966b7d6ef
minho <dnjsalsgh123@gmail.com>
2022-01-06 17:51:04 +0900

    [Java 백준 10808 알파벳 개수]

--- boj/java/src/N_10808/N_10808.java
+++ boj/java/src/N_10808/N_10808.java
@@ -3,9 +3,9 @@ package N_10808;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.List;
+
 import java.util.Scanner;
-import java.util.stream.Collectors;
+
 
 public class N_10808 {
     public static void main(String[] args) throws IOException {
