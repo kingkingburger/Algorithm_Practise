f86dddbd39f084ceabd13b72340d85d7ea5efe41
minho <dnjsalsgh123@gmail.com>
2022-01-30 21:35:29 +0900

    [python 백준 9095 1,2,3더하기]

--- boj/python/9095.py
+++ boj/python/9095.py
@@ -1,17 +1,12 @@
+d = [0] * (1001)
 
-def add(n):
-    
-    if(n<0):
-        return 0
-    if(n==1):
-        return 1
-    elif(n==2):
-        return 2
-    elif(n==3):
-        return 4
+d[1] = 1
+d[2] = 2
+d[3] = 4
+for i in range(4,1001):
+    d[i] = d[i-1] + d[i-2] + d[i-3]
 
-    return add(n-1)+add(n-2)+add(n-3)
 num=int(input())
 for _ in range(num):
     act_num = int(input())
-    print(add(act_num))
+    print(d[act_num])
