3c84804c70bfaf07951f8c76131b6e096741060b
minho <dnjsalsgh123@gmail.com>
2022-01-31 20:03:08 +0900

    [python 백준 11052 카드 구매하기]

---
+++ boj/python/11052.py
@@ -0,0 +1,13 @@
+#구할 카드의 개수
+card_count=int(input())
+
+#카드팩
+card_pack = [0] + list(map(int,input().split()))
+
+for i in range(1,card_count+1):
+    li = [] #카드 개수에 따른 상황들
+    for j in range(i//2+1):
+        li.append(card_pack[i-j]+card_pack[j])
+    card_pack[i] = max(li)
+
+print(max(card_pack))
