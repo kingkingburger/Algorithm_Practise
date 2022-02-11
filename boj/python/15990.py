050f5f1b3e0705eca2a4c06f7034fe7a9682be0c
minho <dnjsalsgh123@gmail.com>
2022-02-11 19:11:10 +0900

    [python 백준 15990 1,2,3 더하기 5]

---
+++ boj/python/15990.py
@@ -0,0 +1,20 @@
+import sys
+
+num=int(sys.stdin.readline())
+
+d = [[0] * 4 for _ in range(100001)]
+
+d[1] = [0,1,0,0]
+d[2] = [0,0,1,0]
+d[3] = [0,1,1,1]
+
+
+for i in range(4,100001):
+    d[i][1] = (d[i-1][2]+d[i-1][3])% 1000000009
+    d[i][2] = (d[i-2][1]+d[i-2][3])% 1000000009
+    d[i][3] = (d[i-3][1]+d[i-3][2])% 1000000009
+
+
+for _ in range(num):
+    act_num = int(sys.stdin.readline())
+    print((sum(d[act_num])) % 1000000009)
