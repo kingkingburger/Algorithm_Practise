bb66dfa1fc955c094162b9f8c3c1efe4889e916d
minho <dnjsalsgh123@gmail.com>
2022-02-14 20:44:13 +0900

    [python 백준 1476 날짜 계산]

---
+++ boj/python/1476.py
@@ -0,0 +1,21 @@
+E, S, M = map(int,input().split())
+
+count_E = 0
+count_S = 0
+count_M = 0
+
+for i in range(1,7980+1):
+    count_E += 1
+    count_S += 1
+    count_M += 1
+    if((count_E % 16) == 0):
+        count_E = 1
+    if((count_S % 29) == 0):
+        count_S = 1
+    if((count_M % 20) == 0):
+        count_M = 1
+    if(count_E == E and count_S == S and count_M == M):
+        print(i)
+        break
+
+        
