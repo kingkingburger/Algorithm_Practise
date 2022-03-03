67a9995e93fbffa257ed307ac048104b6e4ed99d
minho <dnjsalsgh123@gmail.com>
2022-03-03 19:55:28 +0900

    다른거 참고함

--- 프로그래머스/괄호 변환.py
+++ 프로그래머스/괄호 변환.py
@@ -16,40 +16,42 @@ def check(str):
             
 def solution(p):
     answer = ''
+    
     left = 0
     right = 0
+    
     u = ""
     v = ""
-   
+    
     if(len(p) == 0):
         return answer
     
-    for value in range(len(p)): # 몇번 째 인덱스에서 귵형잡힌 괄호가 나오는지
+    for value in range(len(p)+1): 
         if(p[value] =='('):
             left += 1
         elif(p[value] == ')'):
             right += 1
-
+        
         if(left != 0 and left == right):
             u = p[0:value+1]
             v = p[value+1:]
-            print("u:",u,"v",v)
             break
             
     if(check(u)):
         answer += u
         answer += solution(v)
+        
     else:
         answer += '('
         answer += solution(v)
         answer += ')'
-        for q in range(1,len(u)):
-            if(q == '('):
-                answer += ')'
-            if(q == ')'):
-                answer += '('
-    
-    return answer
 
-solution("(()())()")
-solution("()))((()")
+
+        for q in range(1,len(u)-1):
+            if(p[q] == '('):
+                answer += ')'
+            if(p[q] == ')'):
+                answer += '('
+
+        
+    return answer
