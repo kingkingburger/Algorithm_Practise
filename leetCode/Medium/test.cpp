a08d9f982ef8e7f8790998537027c6f19c66092b
minho <dnjsalsgh123@gmail.com>
2025-01-21 09:07:20 +0900

    update: setting cpp compiler in vscode

---
+++ leetCode/Medium/test.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // 표준 입출력 라이브러리를 포함합니다.
+
+int main() {
+    std::cout << "Hello World" << std::endl; // "Hello World"를 출력하고 줄을 바꿉니다.
+    std::cout << "line2" << std::endl;
+    std::cout << "line3" << std::endl;
+    return 0; // 프로그램이 정상적으로 종료되었음을 나타냅니다.
+}
+
