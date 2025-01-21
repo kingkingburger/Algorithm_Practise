0f86b8a214e7b88bfc7e159d21390658f4acb455
minho <dnjsalsgh123@gmail.com>
2025-01-21 10:40:59 +0900

    refactoring: formatting google style for cpp

--- leetCode/Medium/Minumum_Number_of_Operations_to_move_all_balls_to_each_box/Minimum_number_of_operations_to_move_all_balls_to_each_box.cpp
+++ leetCode/Medium/Minumum_Number_of_Operations_to_move_all_balls_to_each_box/Minimum_number_of_operations_to_move_all_balls_to_each_box.cpp
@@ -1,40 +1,38 @@
+#include <cmath>
 #include <iostream>
 #include <vector>
-#include <cmath>
 
 using namespace std;
 
 vector<int> minOperations(string boxes) {
-    vector<int> answer;
-    int n = boxes.size();
+  vector<int> answer;
+  int n = boxes.size();
 
-    for (int i = 0; i < n; i++) {
-        int result = 0;
-
-        for (int j = 0; j < n; j++) {
-            if (i != j && boxes[j] == '1') {
-                result += abs(j - i);
-            }
-        }
-
-        answer.push_back(result);
+  for (int i = 0; i < n; i++) {
+    int result = 0;
+    for (int j = 0; j < n; j++) {
+      if (i != j && boxes[j] == '1') {
+        result += abs(j - i);
+      }
     }
+    answer.push_back(result);
+  }
 
-    return answer;
+  return answer;
 }
 
 int main() {
-    vector<int> result1 = minOperations("110");
-    for (int num : result1) {
-        cout << num << " ";
-    }
-    cout << endl;
+  vector<int> result1 = minOperations("110");
+  for (int num : result1) {
+    cout << num << " ";
+  }
+  cout << endl;
 
-    vector<int> result2 = minOperations("001011");
-    for (int num : result2) {
-        cout << num << " ";
-    }
-    cout << endl;
+  vector<int> result2 = minOperations("001011");
+  for (int num : result2) {
+    cout << num << " ";
+  }
+  cout << endl;
 
-    return 0;
+  return 0;
 }
