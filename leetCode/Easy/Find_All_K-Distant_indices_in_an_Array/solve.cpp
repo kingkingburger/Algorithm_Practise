f9f4ba087cadb575aa08be8111babe2b47468118
wmh <wmh@uvc.co.kr>
2025-06-24 14:33:07 +0900

    docs: findKDistantIndices 추가 설명

--- leetCode/Easy/Find_All_K-Distant_indices_in_an_Array/solve.cpp
+++ leetCode/Easy/Find_All_K-Distant_indices_in_an_Array/solve.cpp
@@ -23,9 +23,28 @@ class Solution {
       }
     }
 
-    sort(result.begin(), result.end());
-
     return result;
+  }
+  vector<int> findKDistantIndices2(vector<int>& nums, int key, int k) {
+    vector<int> ans;
+    int n = nums.size();
+
+    int start = 0;
+    for (int i = 0; i < n; i++) {
+      if (nums[i] == key) {
+        int left = max(0, i - k);
+        int right = min(n - 1, i + k);
+        cout << "left: " << left << ", right: " << right << endl;
+        while (start <= right) {
+          cout << "start: " << start << ", left: " << left << ", right "
+               << right << endl;
+          if (start >= left) ans.push_back(start);
+          start++;
+        }
+      }
+    }
+
+    return ans;
   }
 };
 
@@ -33,8 +52,8 @@ int main() {
   Solution sol;
   vector<int> nums3 = {3, 4, 9, 1, 3, 9, 5};
   vector<int> nums1 = {2, 2, 2, 2, 2};
-  //   auto result = sol.findKDistantIndices(nums3, 9, 1);
-  auto result = sol.findKDistantIndices(nums1, 2, 2);
+  auto result = sol.findKDistantIndices2(nums3, 9, 1);
+  //   auto result = sol.findKDistantIndices2(nums1, 2, 2);
   cout << "result: ";
   for (int i : result) {
     cout << i << " ";
